8961077096fe543ffe7bbf8be5b8c16a3cd27533
Jungho Bang <me@bangtoven.com>
2018-02-25 14:54:22 -0500

    restore record.py

---
+++ record.py
@@ -0,0 +1,22 @@
+from majority import MajorityCheck
+
+class Record:
+    def __init__(self, view, roundNumber, cid, message):
+        self.roundNumber = int(roundNumber)
+        self.view = int(view)  # when it is generated.
+        self.cid = int(cid) # who sent it
+        self.message = message 
+        self.majorityCheck = MajorityCheck()
+        self.learned = False
+
+    @classmethod
+    def fromString(cls, string):
+        parsed = string.split("\t")
+        view = int(parsed[0])
+        roundNumber = int(parsed[1])
+        cid = int(parsed[2])
+        message = parsed[3]
+        return cls(view, roundNumber, cid, message)
+
+    def toString(self):
+        return "{}\t{}\t{}\t{}".format(self.view, self.roundNumber, self.cid, self.message)
